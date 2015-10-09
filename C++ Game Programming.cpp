1e6b7e2c1aa7df519a853f248d31ebb8ea451ff6
John Parayno <johnparayno@John-MacBook-Pro.local>
2015-10-09 12:44:15 +0200

    first commit

---
+++ C++ Game Programming.cpp
@@ -0,0 +1,20 @@
+// C++ Game Programming.cpp : Defines the entry point for the console application.
+//
+
+// *************************************************************  Side XXXX  **********************************************************
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+
+int main()
+{	
+		cout << "\n\n\n\n\n\n\n\n\n\n";
+		cout << "\t\t\t\tHello World!!!!!";
+	
+	cin.get();
+	cin.get();
+	return 0;
+
+}	//end of main function
